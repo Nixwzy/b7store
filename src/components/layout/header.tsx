417e5d02bce193bfd3eeaf8549e41e462fda7716
Nixwzy <guifla330@gmail.com>
2026-03-24 16:56:39 -0300

    feat(header): add background styling and structure to header component

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -2,11 +2,18 @@
 
 export function Header() {
     return (
-        <header className="">
+        <header className="bg-white border-b border-gray-200">
             <div className="bg-black text-white p-4 text-center">
                 <strong>FRETE GRÁTIS</strong> em todo o site para compras acima de R$ 199,00!
             </div>
-            
+            <div className="p-6 w-full max-w-6xl mx-auto">
+                <div className="">
+                    Logo
+                </div>
+                <div className="">
+                    buttons
+                </div>
+            </div>
         </header>
     )
 }
