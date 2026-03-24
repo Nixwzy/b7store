f8cf1b721b9d281e73beac43d083edda6eff8150
Nixwzy <guifla330@gmail.com>
2026-03-24 16:38:16 -0300

    feat(header): update header layout with promotional message and styling

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,7 @@
 export function Header() {
     return (
-        <header>
-            header layout
+        <header className="bg-black text-white p-4 text-center">
+            <strong>FRETE GRÁTIS</strong> em todo o site para compras acima de R$ 199,00!
         </header>
     )
 }
