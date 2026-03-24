c8cd0853df58102d161a64dc534bccc28e8296e5
Nixwzy <guifla330@gmail.com>
2026-03-24 16:52:18 -0300

    feat(header): restructure header component and add promotional message

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,12 @@
+"use client"
+
 export function Header() {
     return (
-        <header className="bg-black text-white p-4 text-center">
-            <strong>FRETE GRÁTIS</strong> em todo o site para compras acima de R$ 199,00!
+        <header className="">
+            <div className="bg-black text-white p-4 text-center">
+                <strong>FRETE GRÁTIS</strong> em todo o site para compras acima de R$ 199,00!
+            </div>
+            
         </header>
     )
 }
