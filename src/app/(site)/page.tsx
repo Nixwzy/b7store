64532bf105a26aeb09e3de7f7a83963a2258d782
Nixwzy <guifla330@gmail.com>
2026-04-04 00:05:38 -0300

    feat(page): add layout structure and image component to the main page

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,10 +1,35 @@
 import { Banners } from "@/components/home/banners";
 import { data } from "@/data";
+import Image from "next/image";
 
 export default function Page () {
   return (
     <div>
       <Banners list={data.banners} />
+      <div className="flex flex-col md:flex-row gap-4 md:gap-8">
+        <div className="flex flex-1 py-6 border border-gray-200 rounded-sm">
+          <div className="w-32 border-r border-gray-200 flex justify-center items-center">
+            <Image
+            src={'/assets/ui/truck-line.png'}
+            alt=""
+            width={40}
+            height={30}
+            />
+          </div>
+          <div className="flex-1">
+            conteudo
+          </div>
+        </div>
+
+         <div className="flex-1">
+          item 2
+        </div>
+
+         <div className="flex-1">
+          item 3
+        </div>
+
+      </div>
     </div>
   )
 }
