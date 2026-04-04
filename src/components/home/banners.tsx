68212988b641d3d528807cceb386697436f515bd
Nixwzy <guifla330@gmail.com>
2026-04-03 23:22:07 -0300

    feat(banners): add opacity transition for active banner in Banners component

--- src/components/home/banners.tsx
+++ src/components/home/banners.tsx
@@ -2,35 +2,41 @@
 import { Banner } from '@/types/banner';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
 
 type Props = {
   list: Banner[];
 };
 export const Banners = ({ list }: Props) => {
+  const [currentImg, setCurrentImg] = useState(0); // state to control which banner is currently visible
+  
   return (
     <div>
-         {/* banner container: relative + aspect-[3/1] defines the height proportionally.
+      {/* banner container: relative + aspect-[3/1] defines the height proportionally.
       All banners are stacked on top of each other using absolute + inset-0.
       Only the active banner (controlled by current state) will be visible via opacity.
       The transition-all enables smooth fade between banners. */}
       <div className="relative aspect-[3/1]">
         {list.map((banner, index) => (
-          <Link key={index} href={banner.link} className='absolute inset-0 transition-all'>
-            <Image 
-            src={banner.img}
-            alt =''
-            width={1200}
-            height={400}
-            className='rounded-md'
+          <Link
+            key={index}
+            href={banner.link}
+            style={{ opacity: currentImg === index ? 1 : 0 }}
+            className="absolute inset-0 transition-all"
+          >
+            <Image
+              src={banner.img}
+              alt=""
+              width={1200}
+              height={400}
+              className="rounded-md"
             />
           </Link>
         ))}
       </div>
       {/* Slide dots: each dot represents a banner.
       Clicking a dot updates the current state, showing the corresponding banner. */}
-      <div>
-        slide dots
-      </div>
+      <div>slide dots</div>
     </div>
   );
 };
