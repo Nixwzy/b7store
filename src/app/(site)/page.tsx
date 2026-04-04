7998b977fa76f10089f2d37535a573690740c88f
Nixwzy <guifla330@gmail.com>
2026-04-04 00:14:47 -0300

    style(page): improve layout and spacing for promotional items

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,35 +1,60 @@
-import { Banners } from "@/components/home/banners";
-import { data } from "@/data";
-import Image from "next/image";
+import { Banners } from '@/components/home/banners';
+import { data } from '@/data';
+import Image from 'next/image';
 
-export default function Page () {
+export default function Page() {
   return (
     <div>
       <Banners list={data.banners} />
-      <div className="flex flex-col md:flex-row gap-4 md:gap-8">
+      <div className="flex flex-col md:flex-row gap-4 md:gap-8 mt-6 md:mt-12">
+
+        {/* item 1 */}
         <div className="flex flex-1 py-6 border border-gray-200 rounded-sm">
           <div className="w-32 border-r border-gray-200 flex justify-center items-center">
             <Image
-            src={'/assets/ui/truck-line.png'}
-            alt=""
-            width={40}
-            height={30}
+              src={'/assets/ui/truck-line.png'}
+              alt=""
+              width={40}
+              height={30}
             />
           </div>
-          <div className="flex-1">
-            conteudo
+          <div className="flex-1 pl-8">
+            <div className="font-bold text-xl">Frete Grátis</div>
+            <div className="text-gray-500">Acima de R$ 199,00</div>
           </div>
         </div>
 
-         <div className="flex-1">
-          item 2
+        {/* item 2 */}
+        <div className="flex flex-1 py-6 border border-gray-200 rounded-sm">
+          <div className="w-32 border-r border-gray-200 flex justify-center items-center">
+            <Image
+              src={'/assets/ui/discount-percent-line.png'}
+              alt=""
+              width={40}
+              height={30}
+            />
+          </div>
+          <div className="flex-1 pl-8">
+            <div className="font-bold text-xl">Ofertas Imbatíveis</div>
+            <div className="text-gray-500">Receba descontos exclusivos</div>
+          </div>
         </div>
-
-         <div className="flex-1">
-          item 3
+        {/* item 3 */}
+        <div className="flex flex-1 py-6 border border-gray-200 rounded-sm">
+          <div className="w-32 border-r border-gray-200 flex justify-center items-center">
+            <Image
+              src={'/assets/ui/arrow-left-right-line.png'}
+              alt=""
+              width={40}
+              height={30}
+            />
+          </div>
+          <div className="flex-1 pl-8">
+            <div className="font-bold text-xl">Troca Fácil</div>
+            <div className="text-gray-500">Devolução em até 30 dias</div>
+          </div>
         </div>
-
       </div>
     </div>
-  )
+  );
 }
