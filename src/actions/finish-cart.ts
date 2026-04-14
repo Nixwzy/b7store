c8850b20f6d9a408860179ea67fa51208132b01a
Nixwzy <guifla330@gmail.com>
2026-04-14 12:27:03 -0300

    feat(cart): implement checkout logic and purchase button actions

---
+++ src/actions/finish-cart.ts
@@ -0,0 +1,9 @@
+'use server';
+
+import { CartItem } from "@/types/cart-item";
+
+export const finishCart = async (token: string, addressId: number, cart: CartItem[]) => {
+    // todo: implement finish cart logic, like calling the API to create an order, etc.
+    return 'https://cdn0.peritoanimal.com.br/pt/posts/7/2/6/como_saber_se_meu_gato_e_feliz_25627_1200.webp';
+    // just a placeholder lol
+};
--- src/components/cart/purchase-button.tsx
+++ src/components/cart/purchase-button.tsx
@@ -1,7 +1,55 @@
+'use client';
+
+import { clearCartCookie } from '@/actions/clear-cart-cookie';
+import { finishCart } from '@/actions/finish-cart';
+import { useAuthStore } from '@/store/auth';
+import { useCartStore } from '@/store/cart';
+import Link from 'next/dist/client/link';
+import { redirect } from 'next/navigation';
+
 export const PurchaseButton = () => {
+  const { token, hydrated } = useAuthStore((state) => state);
+  const cartStore = useCartStore((state) => state);
+
+  const handlePurchase = async () => {
+    if (!token || !cartStore.shipping.selectedAddressId) return;
+
+    const sessionUrl = await finishCart(
+      token,
+      cartStore.shipping.selectedAddressId,
+      cartStore.cart,
+    );
+    if (sessionUrl) {
+      await clearCartCookie();
+      cartStore.clearCart();
+      redirect(sessionUrl);
+    } else {
+      alert(
+        'Ocorreu um erro ao finalizar a compra. Por favor, tente novamente.',
+      );
+    }
+  };
+
+  if (!hydrated) return null;
+
+  if (!token) {
     return (
-        <div className="w-full text-center px-6 py-5 bg-blue-600 text-white border-0 rounded-sm cursor-pointer hover:bg-blue-700 transition-colors duration-300">
-            Finalizar compra
-        </div>
-    )
-}
+      <Link
+        className="block w-full text-center px-6 py-5 bg-blue-600 text-white border-0 rounded-sm cursor-pointer hover:bg-blue-700 transition-colors duration-300"
+        href="/login"
+      >
+        Faça login para finalizar
+      </Link>
+    );
+  }
+
+  return (
+    <button
+      disabled={!cartStore.shipping.selectedAddressId ? true : false}
+      onClick={handlePurchase}
+      className="w-full text-center px-6 py-5 bg-blue-600 text-white border-0 rounded-sm cursor-pointer hover:bg-blue-700 transition-colors duration-300 disabled:opacity-20 disabled:cursor-not-allowed disabled:transition-none"
+    >
+      Finalizar compra
+    </button>
+  );
+};
